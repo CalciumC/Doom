e9a8d98afc6a9c7e18ea68e12e90b6ac76ed507c
Calcium <chancalcium@gmail.com>
2017-01-23 10:57:13 +0800

    relocate flags

--- src/com/doom/flags/CardFlags.java
+++ src/com/doom/flags/CardFlags.java
@@ -1,6 +1,6 @@
-package com.doom.utils;
+package com.doom.flags;
 
-public class Flags {
+public class CardFlags {
 
 	public final static String ITEM_TYPE_FOOD = "F";
 	public final static String ITEM_TYPE_WEAPON = "W";
@@ -11,9 +11,6 @@ public class Flags {
 	public final static String ITEM_TYPE_WEAPON_TEXT = "WEAPONS";
 	public final static String ITEM_TYPE_TOOL_TEXT = "TOOLS";
 	public final static String ITEM_TYPE_JUNK_TEXT = "JUNKS";
-	
-	public final static Integer GAME_ROUNDS_LIMIT = 7;
-	public final static Integer GAME_PHASES_LIMIT = 3;
 	
 	public final static String DOOM_TYPE_BOSS = "B";
 	public final static String DOOM_TYPE_HEAVEN = "H";
---
+++ src/com/doom/flags/GameFlags.java
@@ -0,0 +1,8 @@
+package com.doom.flags;
+
+public class GameFlags {
+
+	public final static Integer GAME_ROUNDS_LIMIT = 7;
+	public final static Integer GAME_PHASES_LIMIT = 3;
+
+}
