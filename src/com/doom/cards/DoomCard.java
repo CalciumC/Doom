b130ce1ad5490fc295d82ab25d9f9cea0c24d1dd
Calcium <chancalcium@gmail.com>
2017-01-23 14:11:25 +0800

    checkDoom() in progress

--- src/com/doom/cards/DoomCard.java
+++ src/com/doom/cards/DoomCard.java
@@ -1,5 +1,10 @@
 package com.doom.cards;
 
+import java.util.ArrayList;
+
+import com.doom.flags.CardFlags;
+import com.doom.player.Player;
+
 public class DoomCard extends Card {
 
 	private String doomType;
@@ -15,6 +20,20 @@ public class DoomCard extends Card {
 	public DoomCard(String cardName, String cardDesc, String cardImgPath) {
 		super(cardName, cardDesc, cardImgPath);
 		// TODO Auto-generated constructor stub
+	}
+	
+	public boolean checkDoom(ArrayList<Player> players){
+		// TODO Finish doom
+		switch(doomType){
+			case CardFlags.DOOM_TYPE_BOSS:
+			case CardFlags.DOOM_TYPE_DOOM:
+			case CardFlags.DOOM_TYPE_TEAM:
+			case CardFlags.DOOM_TYPE_HEAVEN:
+				return true;
+			
+		}
+		
+		return false;
 	}
 
 }
