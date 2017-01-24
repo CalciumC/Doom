39e3a9eefaa2fe7dc8ec8605bfe8c87e2726896a
Calcium <chancalcium@gmail.com>
2017-01-24 14:35:21 +0800

    add printTitle() and printMsg()[WIP]

--- src/com/doom/utils/Common.java
+++ src/com/doom/utils/Common.java
@@ -2,6 +2,8 @@ package com.doom.utils;
 
 import java.util.ArrayList;
 
+import com.doom.flags.UtilsFlags;
+
 public class Common {
 	
 	private static final Common instance = new Common();
@@ -44,5 +46,55 @@ public class Common {
 		
 	}
 	
-
+	public String printTitle(String title){
+		
+		return printTitle(title, UtilsFlags.TITLE_LENGTH);
+	}
+	
+	public String printTitle(String title, int maxLen){
+		String prefixsuffix = UtilsFlags.TITLE_PREFIX_SUFFIX;
+		int maxLength = maxLen; 
+		
+		title = " " + title + " ";
+		
+		int length = title.length() + prefixsuffix.length() * 2;
+		
+		if(length > maxLength){
+			int extraDigit = length - maxLength;
+			int toCut = extraDigit/2;
+			String fillSpace = "";
+			if(extraDigit % 2 == 1){
+				toCut++;
+				fillSpace = " ";
+			}
+			
+			prefixsuffix = prefixsuffix.substring(0, prefixsuffix.length()- toCut);
+			title = title + fillSpace;
+			
+		}else if(length < maxLength){
+			int toFill = maxLength - length;
+			if(toFill == 1){
+				title = title + " ";
+			}else if(toFill >1){
+				for(int i=0; i < toFill/2; i++){
+					prefixsuffix = prefixsuffix + UtilsFlags.TITLE_FILLING;
+				}
+				if(toFill % 2  == 1){ 
+					title = title + " ";
+				}
+			}
+		}
+	
+		return prefixsuffix + title + prefixsuffix;
+	}
+	
+	public String printMsg(int type, String[] param){
+		
+		switch(type){
+			//TODO case intType to msg 
+		
+		}
+		
+		return "";
+	}
 }
